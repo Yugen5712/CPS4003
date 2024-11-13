5c57b0301fec419bc67b65e61e661b98480c9dbd
Yugen Limbu <2407570@live.stmarys.ac.uk>
2024-11-13 16:39:54 +0000

    Added all the are functions

--- files/calculate.py
+++ files/calculate.py
@@ -1,14 +1,46 @@
 import math
 import supportingFunctions
 
+def square_area():
+    side = int(input("Enter the length of the sides."))
+    return square_area
+
+def retriangle_area():
+    length = int(input("Enter the length of the side: "))
+    width = int(input("Enter the width: "))
+    return length*width
+
+def circle_area():
+    radius = int(input("Enter the radius: "))
+    return math.pi*(radius**2)
+
+def sphere_area():
+    radius = int(input("Enter the raidus: "))
+    return 4*math.pi*radius**2
+
+def cone_area():
+    radius = int(input("Enter the raidus: "))
+    length = int(input("Enter the length: "))
+    return math.pi*radius*(radius + length)
+
+def cylinder_area():
+    radius = int(input("Enter the radius: "))
+    height = int(input("Enter the height: "))
+    return 2*(math.pi*(radius**2)) + height*(2*math.pi*radius)
+
 def area_menue():
     menue = """
 1. Square area.
 2. Rectriangle area.
 3. Circle area.
 4. Sphere area.
+6. Cylinder area.
 5. Cone area.
 """
+    option = int(input(menue))
+    option = Invalid_option(option, 5, menue)
+    if option == 1:
+        square_area()
 
 def perimeter_menue():
     pass
